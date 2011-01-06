baab3dd1c3804707e3a450c4060ffa4922791ae4
Justin Ryan <justin@halfempty.org>
2011-01-06 15:42:27 -0500

    Move to MUC and xmpp.halfempty.org

--- PongBot.py
+++ PongBot.py
@@ -25,6 +25,7 @@ class PongBot(sleekxmpp.ClientXMPP):
         self._server=config.get('XMPP', 'server')
         self._port=config.getint('XMPP', 'port')
         self._to=config.get('XMPP', 'to')
+        self._useMUC=config.getboolean('XMPP', 'useMUC')
 
         logging.debug("JID is %s" % self._jid)
         logging.debug("Password is %s" % self._password)
@@ -32,6 +33,7 @@ class PongBot(sleekxmpp.ClientXMPP):
 
         self.add_event_handler("session_start", self._session_start)
         self.add_event_handler("message", self._handleIncoming) 
+        self.add_event_handler("groupchat_message", self._handleIncomingGroupChatMessage) 
         self.add_event_handler("sent_presence", self._handlePresence) 
         self.registerPlugin('xep_0030') # Service Discovery
         self.registerPlugin('xep_0004') # Data Forms
@@ -48,9 +50,23 @@ class PongBot(sleekxmpp.ClientXMPP):
             self.process(threaded=True)
             logging.info("Processing started")
             self._evt.wait()
+
         else:
             logging.error("Unable to connect")
             self._evt.set()
+
+    def joinRoom(self):
+        #initialize and handle Group Chat room 
+        muc = self.plugin['xep_0045'] 
+        logging.info( "trying to Join room" )
+        room = self._to
+        join = muc.joinMUC(room, self._jid) 
+        print 'join: ', join, '\n' 
+        #if join == 'True': 
+        #    conf = muc.getRoomConfig('score') 
+        #    print 'conf: ', join, '\n' 
+        #    form = muc.getRoomForm('%s@%s' % ('score','muc')) 
+        #    print 'form: ', form, '\n' 
 
     def _session_start(self, start):
         self.getRoster()
@@ -59,10 +75,17 @@ class PongBot(sleekxmpp.ClientXMPP):
 
     def _handlePresence(self, presence):
         logging.info("Receved presence")
+        if self._useMUC:
+            self.joinRoom()
         self._evt.set()
 
     def _handleIncoming(self, msg):
-        msg.reply("Pong").send()
+        logging.info("Message created %s" % msg)
+        #msg.reply("Pong").send()
+
+    def _handleIncomingGroupChatMessage(self, msg):
+        logging.info("Group Message created %s" % msg)
+        #msg.reply("Pong").send()
 
     def pointLeft(self, dict):
         self._post(dict, "Point Left")
@@ -82,7 +105,11 @@ class PongBot(sleekxmpp.ClientXMPP):
             move: Sequence of move made
         """
         logging.info("sending %s to %s" % (verb, self._to))
-        self.sendMessage(self._to, "%s in %d[%d] %s vs %s" % (verb, dict['game'], dict['move'], dict['left'], dict['right']), mtype='chat')
+        if self._useMUC:
+            mtype = 'groupchat'
+        else:
+            mtype = 'chat'
+        self.sendMessage(self._to, "%s in %d[%d] %s vs %s" % (verb, dict['game'], dict['move'], dict['left'], dict['right']), mtype=mtype)
         
 if __name__ == '__main__':
 
